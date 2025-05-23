cb737ac20c2f6c0d20fd70df35b1c98fd5a41b68
Elian Garcia <telljua90@gmail.com>
2025-05-23 14:26:42 -0600

    componente app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import "./App.css";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import InicioLayout from "./layouts/InicioLayout";
 import Inicio from "./pages/Inicio";
 import NotFound from "./pages/NotFound";
@@ -13,8 +10,37 @@ import HConsejo from "./pages/HConsejo";
 import AdministracionCentral from "./pages/AdministracionCentral";
 import ValoresUagro from "./pages/ValoresUagro";
 import Deportes from "./pages/Deportes";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getSchools, getServices } from "./redux/actions/EduActions";
+import {
+  getFeaturedSports,
+  getSports,
+  getTalents,
+} from "./redux/actions/SportsActions";
+import { statusHttp } from "./redux/reducers/EduReducer";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const schoolStatus = useSelector((state) => state.school.schoolsState.status);
+  const sportsStatus = useSelector((state) => state.sports.sportState.status);
+
+  useEffect(() => {
+    if (schoolStatus === statusHttp.IDLE) {
+      dispatch(getSchools());
+      dispatch(getServices());
+    }
+  }, [dispatch, schoolStatus]);
+
+  useEffect(() => {
+    if (sportsStatus === statusHttp.IDLE) {
+      dispatch(getSports());
+      dispatch(getTalents());
+      dispatch(getFeaturedSports());
+    }
+  }, [dispatch, sportsStatus]);
+
   const router = createBrowserRouter([
     {
       path: "/frontend",
--- src/layouts/InicioLayout.jsx
+++ src/layouts/InicioLayout.jsx
@@ -1,37 +1,8 @@
 import { Outlet, ScrollRestoration } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getSchools, getServices } from "../redux/actions/EduActions";
-import {
-  getFeaturedSports,
-  getSports,
-  getTalents,
-} from "../redux/actions/SportsActions";
-import { statusHttp } from "../redux/reducers/EduReducer";
 
 export default function InicioLayout() {
-  const dispatch = useDispatch();
-
-  const schoolStatus = useSelector((state) => state.school.schoolsState.status);
-  const sportsStatus = useSelector((state) => state.sports.sportState.status);
-
-  useEffect(() => {
-    if (schoolStatus === statusHttp.IDLE) {
-      dispatch(getSchools());
-      dispatch(getServices());
-    }
-  }, [dispatch, schoolStatus]);
-
-  useEffect(() => {
-    if (sportsStatus === statusHttp.IDLE) {
-      dispatch(getSports());
-      dispatch(getTalents());
-      dispatch(getFeaturedSports());
-    }
-  }, [dispatch, sportsStatus]);
-
   return (
     <>
       <Header />
